fe286a160b5da734fb4d980810575ce33336fc6d
alejandrodh <alejandro@digitalhouse.com>
2021-05-03 12:50:28 -0300

    fix en nombre de atributo  tableName

--- clase10_sequlize/udesaMovies/database/models/Movie.js
+++ clase10_sequlize/udesaMovies/database/models/Movie.js
@@ -31,7 +31,7 @@ module.exports = function(sequelize, dataTypes){
     }
 
     let config = {
-        table: 'movies', 
+        tableName: 'movies', 
         timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
         underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
     }
